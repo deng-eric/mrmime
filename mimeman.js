dc8873a065e613213cb9000c05340824c8c530dd
Eric <eric.deng@mail.utoronto.ca>
2015-07-01 13:53:26 -0400

    goods

---
+++ mimeman.js
@@ -0,0 +1,9 @@
+
+<script type="text/javascript">	
+	function myFunction() {
+    document.getElementById("audio").play();
+	}
+
+	var a=document.getElementById("swag");
+	a.addEventListener("click", myFunction)
+</script>
