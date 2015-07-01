ef22636c9a084cb9f687374da353c8fe0c0598c7
Eric <eric.deng@mail.utoronto.ca>
2015-07-01 18:17:56 -0400

    update boys

--- mimeman.js
+++ mimeman.js
@@ -1,9 +1,21 @@
 
-<script type="text/javascript">	
-	function myFunction() {
+  window.onload = shiny;
+
+  function shiny(){
+    randomNum = Math.floor((Math.random() * 10));
+    var w;
+    if(randomNum == 0){
+      w = "RARE.gif"
+    }
+    else{
+      w = "mr-mime.gif"
+    }
+    document.getElementById("swag").src = w;
+  }
+
+	function cryin() {
     document.getElementById("audio").play();
 	}
 
-	var a=document.getElementById("swag");
-	a.addEventListener("click", myFunction)
-</script>
+	var a = document.getElementById("swag");
+	a.addEventListener("click", cryin);
